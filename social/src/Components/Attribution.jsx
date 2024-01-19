9b9e7e20ce211bf9427a8857aaff4a6ec34a38a5
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2024-01-19 21:13:42 +0530

    setup

---
+++ social/src/Components/Attribution.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Attribution() {
+  return (
+    <div className="attribution m-[1rem]">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by <a href="https://www.frontendmentor.io/profile/IndranjanaChatterjee">IndranjanaChatterjee</a>.
+  </div>
+  )
+}
