8c747e9ce861bdf4c04e8964b22cc93efd687adb
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2024-01-20 09:15:18 +0530

    design

--- social/src/Components/Card.jsx
+++ social/src/Components/Card.jsx
@@ -1,21 +1,31 @@
-import React from 'react';
-import pic from "../assets/images/avatar-jessica.jpeg"
+import React from "react";
+import pic from "../assets/images/avatar-jessica.jpeg";
 
 export default function Card() {
   return (
-    <div className='p-[2rem] bg-[#1f1f1f] rounded-[0.7rem]'>
-      <div className="inside  flex flex-col justigy-center items-center w-[20rem] h-[30rem] ">
-        <img src={pic} alt="pic" className='w-[6rem] h-[6rem] rounded-full'/>
-        <p>Jessica Randall</p>
-  <p>London, United Kingdom</p>
-  <p>"Front-end developer and avid reader."</p> 
+    <div className="p-[2rem] bg-[#1f1f1f] rounded-[0.7rem]">
+      <div className="inside  flex flex-col justigy-center items-center w-[20rem] h-[35rem] ">
+        <img src={pic} alt="pic" className="w-[6rem] h-[6rem] rounded-full mb-[1rem]" />
+        <p className="text-[#ffffff] text-[1.8rem] font-semibold">Jessica Randall</p>
+        <p className="text-[#c5f82a] font-semibold mb-[1.5rem]">London, United Kingdom</p>
+        <p className="text-[#ffffff] font-semibold mb-[1rem]">"Front-end developer and avid reader."</p>
 
-  <button className='bg-[#333333] hover:bg-[#c5f82a] transition-all duration-300 ease-linear m-[0.5rem] w-[100%] text-center h-[3rem] rounded-[0.5rem] '>GitHub</button>
-  <button className='bg-[#333333] hover:bg-[#c5f82a] transition-all duration-300 ease-linear m-[0.5rem] w-[100%] text-center h-[3rem] rounded-[0.5rem] '>Frontend Mentor</button>
-  <button className='bg-[#333333] hover:bg-[#c5f82a] transition-all duration-300 ease-linear m-[0.5rem] w-[100%] text-center h-[3rem] rounded-[0.5rem] '>LinkedIn</button>
-  <button className='bg-[#333333] hover:bg-[#c5f82a] transition-all duration-300 ease-linear m-[0.5rem] w-[100%] text-center h-[3rem] rounded-[0.5rem] '>Twitter</button>
-  <button className='bg-[#333333] hover:bg-[#c5f82a] transition-all duration-300 ease-linear m-[0.5rem] w-[100%] text-center h-[3rem] rounded-[0.5rem] '>Instagram</button>
+        <button className="bg-[#333333] hover:bg-[#c5f82a]  hover:text-[#141414] font-bold transition-all duration-300 ease-linear m-[0.5rem] w-[100%] text-center h-[3rem] rounded-[0.5rem] text-[#ffffff]">
+          GitHub
+        </button>
+        <button className="bg-[#333333] hover:bg-[#c5f82a] hover:text-[#141414] font-bold transition-all duration-300 ease-linear m-[0.5rem] w-[100%] text-center h-[3rem] rounded-[0.5rem] text-[#ffffff]">
+          Frontend Mentor
+        </button>
+        <button className="bg-[#333333] hover:bg-[#c5f82a] hover:text-[#141414] font-bold transition-all duration-300 ease-linear m-[0.5rem] w-[100%] text-center h-[3rem] rounded-[0.5rem] text-[#ffffff]">
+          LinkedIn
+        </button>
+        <button className="bg-[#333333] hover:bg-[#c5f82a] hover:text-[#141414] font-bold transition-all duration-300 ease-linear m-[0.5rem] w-[100%] text-center h-[3rem] rounded-[0.5rem] text-[#ffffff]">
+          Twitter
+        </button>
+        <button className="bg-[#333333] hover:bg-[#c5f82a] hover:text-[#141414] font-bold transition-all duration-300 ease-linear m-[0.5rem] w-[100%] text-center h-[3rem] rounded-[0.5rem] text-[#ffffff]">
+          Instagram
+        </button>
       </div>
     </div>
-  )
+  );
 }
