c20a1e99983606826b40d8f6c6d11df5289558b8
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2024-01-20 09:20:07 +0530

    responsiveness

--- social/src/Components/Card.jsx
+++ social/src/Components/Card.jsx
@@ -3,12 +3,12 @@ import pic from "../assets/images/avatar-jessica.jpeg";
 
 export default function Card() {
   return (
-    <div className="p-[2rem] bg-[#1f1f1f] rounded-[0.7rem]">
-      <div className="inside  flex flex-col justigy-center items-center w-[20rem] h-[35rem] ">
+    <div className="p-[2rem] bg-[#1f1f1f] rounded-[0.7rem] m-[1rem]">
+      <div className="inside  flex flex-col justigy-center items-center sm:w-[20rem] sm:h-[35rem] ">
         <img src={pic} alt="pic" className="w-[6rem] h-[6rem] rounded-full mb-[1rem]" />
-        <p className="text-[#ffffff] text-[1.8rem] font-semibold">Jessica Randall</p>
-        <p className="text-[#c5f82a] font-semibold mb-[1.5rem]">London, United Kingdom</p>
-        <p className="text-[#ffffff] font-semibold mb-[1rem]">"Front-end developer and avid reader."</p>
+        <p className="text-[#ffffff] text-[1.8rem] font-semibold text-center">Jessica Randall</p>
+        <p className="text-[#c5f82a] font-semibold mb-[1.5rem] text-center">London, United Kingdom</p>
+        <p className="text-[#ffffff] font-semibold mb-[1rem] text-center">"Front-end developer and avid reader."</p>
 
         <button className="bg-[#333333] hover:bg-[#c5f82a]  hover:text-[#141414] font-bold transition-all duration-300 ease-linear m-[0.5rem] w-[100%] text-center h-[3rem] rounded-[0.5rem] text-[#ffffff]">
           GitHub
